b8206ededfb5ac9230f06da5540628cb1b2e39da
Alexander Junge <alexander.junge@gmail.com>
2020-07-04 15:27:13 +0200

    First example

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# Running in a container
+
+```
 docker build --tag nlp_app:1.0 .
 docker run -p 8501:8501 nlp_app:1.0
 # docker run -it -p 8501:8501 nlp_app:1.0 /app/bin/example.py
+```
+
+# Running locally
+
+```
+python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_md
+```
--- nlp_app/example.py
+++ nlp_app/example.py
@@ -1,14 +1,15 @@
-"""
-Source: https://github.com/explosion/spacy-streamlit
-Very basic out-of-the-box example using the full visualizer. This file can be
-run using the "streamlit run" command.
-
-Prerequisites:
-python -m spacy download en_core_web_sm
-python -m spacy download en_core_web_md
-"""
+import requests
 import spacy_streamlit
+import streamlit as st
 
 models = ["en_core_web_sm", "en_core_web_md"]
-default_text = "Sundar Pichai is the CEO of Google."
+resp = requests.get(
+    "https://p55oroem7k.execute-api.eu-west-1.amazonaws.com/prod/articles/?skip=85&limit=20"
+).json()
+id_to_text = {}
+for d in resp:
+    id_to_text[d["id"]] = (d["title"].strip() + " " + d["summary"]).strip()
+
+pmid = st.selectbox('Select article', sorted(id_to_text.keys()))
+default_text = id_to_text[pmid]
 spacy_streamlit.visualize(models, default_text)
