bfd1c852c8f18ea69f4cdf40a8c039bf7b7213fd
Alexander Junge <alexander.junge@gmail.com>
2020-12-22 15:16:24 +0100

    Query local document API

--- nlp_app/example.py
+++ nlp_app/example.py
@@ -3,13 +3,11 @@ import spacy_streamlit
 import streamlit as st
 
 models = ["en_core_web_sm", "en_core_web_md"]
-resp = requests.get(
-    "https://p55oroem7k.execute-api.eu-west-1.amazonaws.com/prod/articles/?skip=85&limit=20"
-).json()
+resp = requests.get("http://127.0.0.1:8000/documents/?skip=0&limit=5").json()
 id_to_text = {}
 for d in resp:
-    id_to_text[d["id"]] = (d["title"].strip() + " " + d["summary"]).strip()
+    id_to_text[d["id"]] = (d["title"].strip() + "\n" + d["parsed_text"]).strip()
 
-pmid = st.selectbox('Select article', sorted(id_to_text.keys()))
-default_text = id_to_text[pmid]
-spacy_streamlit.visualize(models, default_text)
+id_ = st.selectbox("Select document", sorted(id_to_text.keys()))
+default_text = id_to_text[id_]
+spacy_streamlit.visualize(models, default_text, visualizers=["ner"])
