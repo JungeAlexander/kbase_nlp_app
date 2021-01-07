49e1b405c89d56bc74a6978a2dff6a4ec43740ff
Alexander Junge <alexander.junge@gmail.com>
2021-01-07 16:44:11 +0100

    Add text box to search

--- nlp_app/example.py
+++ nlp_app/example.py
@@ -10,13 +10,18 @@ load_dotenv()
 token_headers = {"Authorization": f"Bearer {os.environ['ACCESS_TOKEN']}"}
 
 models = ["en_core_web_sm", "en_core_web_md"]
+
+search_query = st.text_input("Input your search query here:", "query")
 resp = requests.get(
-    "http://127.0.0.1:8000/documents/?skip=0&limit=5", headers=token_headers
+    f"http://127.0.0.1:8000/documents/search_summary?query={search_query}",
+    headers=token_headers,
 ).json()
 id_to_text = {}
 for d in resp:
     id_to_text[d["id"]] = (d["title"].strip() + "\n" + d["parsed_text"]).strip()
 
 id_ = st.selectbox("Select document", sorted(id_to_text.keys()))
-#default_text = id_to_text[id_]
-#spacy_streamlit.visualize(models, default_text, visualizers=["ner"])
+
+default_text = id_to_text[id_]
+st.text(default_text)
+# spacy_streamlit.visualize(models, default_text, visualizers=["ner"])
