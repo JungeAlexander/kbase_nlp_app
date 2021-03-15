38559e87594d56be755529bfc63b3ec7f61408f4
Alexander Junge <alexander.junge@gmail.com>
2021-03-15 20:11:35 +0100

    Use API URL from .env file, run vanilla NER

--- nlp_app/example.py
+++ nlp_app/example.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 
 from dotenv import load_dotenv
 import requests
@@ -8,12 +9,13 @@ import streamlit as st
 load_dotenv()
 
 token_headers = {"Authorization": f"Bearer {os.environ['ACCESS_TOKEN']}"}
+api_url = urllib.parse.urljoin(os.environ["DBAPI_URL"], os.environ["DBAPI_STAGE"])
 
 models = ["en_core_web_sm", "en_core_web_md"]
 
 search_query = st.text_input("Input your search query here:", "query")
 resp = requests.get(
-    f"http://127.0.0.1:8000/documents/search_summary?query={search_query}",
+    f"{api_url}/documents/search_summary?query={search_query}",
     headers=token_headers,
 ).json()
 id_to_text = {}
@@ -24,4 +26,4 @@ id_ = st.selectbox("Select document", sorted(id_to_text.keys()))
 
 default_text = id_to_text[id_]
 st.text(default_text)
-# spacy_streamlit.visualize(models, default_text, visualizers=["ner"])
+spacy_streamlit.visualize(models, default_text, visualizers=["ner"])
